ae3b9832a1e18459d7139da04b47c773bbce1b13
Roman Serdiuk <roman.srdk21@gmail.com>
2026-06-22 12:38:43 +0300

    init

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
+  },
+]);
