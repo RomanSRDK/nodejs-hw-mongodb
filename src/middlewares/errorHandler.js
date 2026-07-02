9f73c0198ceafaa7439bd7c326a1b2c16c4794bf
Roman Serdiuk <100283300+RomanSRDK@users.noreply.github.com>
2026-07-02 16:44:13 +0300

    Enhance errorHandler to return detailed error messages
    Updated error handling to include detailed error messages.

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,8 +4,8 @@ export const errorHandler = (err, req, res, next) => {
   if (err instanceof createHttpError.HttpError) {
     return res.status(err.status).json({
       status: err.status,
-      message: err.name,
-      data: err.message,
+      message: err.message,
+      errors: err.errors?.map((error) => error.message),
     });
   }
 
